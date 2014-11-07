9b619f7bb27a8fc3abc25bb56e1a38450cd8aa14
xjplke <juwu.shao@gmail.com>
2014-11-07 11:29:51 +0800

    first edition

---
+++ src/main/java/me/itsoho/shiro/cluster/listener/CustomSessionListener.java
@@ -0,0 +1,37 @@
+package me.itsoho.shiro.cluster.listener;
+
+import me.itsoho.shiro.cluster.session.ShiroSessionRepository;
+
+import org.apache.shiro.session.Session;
+import org.apache.shiro.session.SessionListener;
+
+public class CustomSessionListener implements SessionListener {
+
+    private ShiroSessionRepository shiroSessionRepository;
+
+    @Override
+    public void onStart(Session session) {
+        //TODO
+        System.out.println("on start");
+    }
+
+    @Override
+    public void onStop(Session session) {
+        //TODO
+        System.out.println("on stop");
+    }
+
+    @Override
+    public void onExpiration(Session session) {
+        shiroSessionRepository.deleteSession(session.getId());
+    }
+
+    public ShiroSessionRepository getShiroSessionRepository() {
+        return shiroSessionRepository;
+    }
+
+    public void setShiroSessionRepository(ShiroSessionRepository shiroSessionRepository) {
+        this.shiroSessionRepository = shiroSessionRepository;
+    }
+
+}
